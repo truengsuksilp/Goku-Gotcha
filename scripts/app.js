eb7f7f129e9329efcda91275f2e43375f4790336
truengsuksilp <truengsuksilp@gmail.com>
2021-09-28 11:19:20 -0700

    Landing Page linked to Bootstrap and internal CSS and JS

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+console.log('[app.js] Loaded')
+
+// Approach: Use Objects and Functions
+
+
+/* === Variables: Global === */
+
+/* === Dom Elements  === */
+
+/* === Variables: Objects with Methods === */
+
+/* === Event Functions  === */
+
+/* === Event Listeners === */
+
+/* === Invoked Functions === */
